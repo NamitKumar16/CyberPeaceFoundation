08f4852d70f9194ba7460e0a5de9fb37bade229c
NamitKumar16 <kumarnamit58@gmail.com>
2022-06-01 12:08:38 +0530

    setting up handlebars

--- Task 1/src/app.js
+++ Task 1/src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-require("dotenv").config()
+const hbs = require("hbs");
+require("dotenv").config();
 require("./db/conn");
 
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname, "../public");
-app.use(express.static(static_path));
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path)
 
 app.get("/", (req, res) => {
-  res.send("Hello All");
+  res.render("index");
 });
 
 app.listen(port, () => {
